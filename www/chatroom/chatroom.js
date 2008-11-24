519969811e4803fd81bf6f8558ed37ae44bdbdfa
francesco <francesco@valhalla.(none)>
2008-11-24 18:57:06 +0100

    some compatibility corrections, added until-test.ehwas

--- www/chatroom/chatroom.js
+++ www/chatroom/chatroom.js
@@ -1,11 +1,10 @@
 with (Actors) with (ActorsMatch) with (ActorsTest) with (ActorsRemote) {
 
-    ActorsRemote.init ();
+    var name, chatroom, main_actor;
 
-    var chatroom = get ("/registry/chatroom");
-    var name;
     var main = function () {
 	name = prompt ("What's your nickname?");
+	if (! name) throw "without a nick you cannot enter in the chatroom";
 	send (chatroom, {"enter": {"name": name, "address": self ()}});
 	recv (
 	    cond(
@@ -72,10 +71,24 @@ with (Actors) with (ActorsMatch) with (ActorsTest) with (ActorsRemote) {
     }
 
     var sendMessage = function () {
-	send (chatroom, {"publish": 
-			 {"from": name,
-			  "body": document.getElementById ("message").value}});
+	var msg = document.getElementById ("message").value;
+	if (msg.length) send (
+	    chatroom, 
+	    {"publish": 
+	     {"from": name,
+	      "body": msg}});
 	document.getElementById("message").value = "";
     }
-    var main_actor = actor (main);
+    
+    window.onload = function () {
+	ActorsRemote.init ();
+	chatroom = get ("/registry/chatroom");
+	main_actor = actor (main);
+    };
+
+    window.onunload = function () {
+	alert ("exiting");
+	send (chatroom, {'exit': {'name': name, 'address' : main_actor}});
+    };
+    
 }// end of with (Actors) ...
