498293d7946afdc175d628cad573741762c1606d
francesco <francesco@bethesda.(none)>
2009-12-31 06:25:28 +0100

    refactor: ehwas update moved pg-sessions and file sessions in ehwas/sessions subdir

---
+++ ehwas/sessions/file-sessions#.scm
@@ -0,0 +1,8 @@
+(##namespace
+ ("ehwas-sessions-file-sessions#"
+  file-session-dir
+  file-session-driver
+  ))
+
+;; (include "ehwas-sessions#.scm")
+;; (current-session-driver file-session-driver)
