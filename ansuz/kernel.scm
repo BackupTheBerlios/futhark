c58a48db7abced0b5b89ea608c1190718e14220a
francesco bracchi <frbracch@gmail.com>
2010-01-08 08:49:24 +0100

    added token source to parser exception

--- ansuz/kernel.scm
+++ ansuz/kernel.scm
@@ -12,19 +12,19 @@
          ;;(not safe)
          )
 
-(define-structure parser-exception reason)
+(define-structure parser-exception reason source)
 
 (define-parser (char c0)
   (reflect (ts sc fl)
            (let(
                 (c (source-car ts)))
-             
-           (if (and (char? c) (char=? (source-car ts) c0))
-               (sc c0 (source-cdr ts) fl)
-               (fl (make-parser-exception
-                    (string-append
-                     "not "
-                     (list->string (list c0)))))))))
+             (if (and (char? c) (char=? (source-car ts) c0))
+                 (sc c0 (source-cdr ts) fl)
+                 (fl (make-parser-exception
+                      (string-append
+                       "not "
+                       (list->string (list c0)))
+                      ts))))))
 
 
 (define-parser (digit)
@@ -33,7 +33,7 @@
                 (c (source-car ts)))
              (if (and (char? c) (char-numeric? c))
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "not a digit"))))))
+                 (fl (make-parser-exception "not a digit" ts))))))
 
 (define-parser (upcase)
   (reflect (ts sc fl)
@@ -41,7 +41,7 @@
                 (c (source-car ts)))
              (if (and (char? c) (char>=? c #\A) (char<=? c #\Z))
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "not an upcase"))))))
+                 (fl (make-parser-exception "not an upcase" ts))))))
 
 (define-parser (locase)
   (reflect (ts sc fl)
@@ -49,7 +49,7 @@
                 (c (source-car ts)))
              (if (and (char? c) (char>=? c #\a) (char<=? c #\z))
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "not a locase"))))))
+                 (fl (make-parser-exception "not a locase" ts))))))
 
 (define-parser (interval l u)
   (reflect (ts sc fl)
@@ -57,7 +57,7 @@
                 (c (source-car ts)))
              (if (and (char? c) (char>=? c l) (char<=? c u))
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "not in interval"))))))
+                 (fl (make-parser-exception "not in interval" ts))))))
 
 (define-parser (alpha)
   (reflect (ts sc fl)
@@ -65,7 +65,7 @@
                 (c (source-car ts)))
              (if (and (char? c) (char-alphabetic? c))
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "not an alpha"))))))
+                 (fl (make-parser-exception "not an alpha" ts))))))
 
 (define-parser (whitespace)
   (reflect (ts sc fl)
@@ -73,13 +73,13 @@
                 (c (source-car ts)))
              (if (and (char? c) (char-whitespace? c))
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "not a whitespace"))))))
+                 (fl (make-parser-exception "not a whitespace" ts))))))
 
 (define-parser (eos)
   (reflect (ts sc fl)
            (if (eof-object? (source-car ts))
                (sc #!eof (source-cdr ts) fl)
-               (fl (make-parser-exception "not end")))))
+               (fl (make-parser-exception "not end" ts)))))
 
 (define-parser (any)
   (reflect (ts sc fl)
@@ -87,7 +87,7 @@
                 (c (source-car ts)))
              (if (char? c)
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "not any"))))))
+                 (fl (make-parser-exception "not any" ts))))))
 
 (define-parser (test-token t?)
   (reflect (ts sc fl)
@@ -95,11 +95,11 @@
                 (c (source-car ts)))
              (if (and (char? c) (t? c))
                  (sc c (source-cdr ts) fl)
-                 (fl (make-parser-exception "test failed"))))))
+                 (fl (make-parser-exception "test failed" ts))))))
 
 (define-parser (fail r)
   (reflect (ts sc fl)
-           (fl (make-parser-exception r))))
+           (fl (make-parser-exception r ts))))
 
 (define-parser (token-source)
   (reflect (ts sc fl) (sc ts ts fl)))
