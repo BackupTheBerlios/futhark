1f6d5c132e18855d42bcdc35d8450754c1a9440f
francesco bracchi <francesco@marduk.(none)>
2010-10-11 21:32:41 +0200

    some changes to the way delays are treated by port streams (and port streams with position)

--- src/ansuz/sources/port#.scm
+++ src/ansuz/sources/port#.scm
@@ -6,12 +6,53 @@
               stream-col
               stream))
 
+;; (define-macro (stream-car p)
+;;   `(force (car ,p)))
+
+;; (define-macro (stream-cdr p)
+;;   `(force (cdr ,p)))
+
+;; (define-macro (stream p)
+;;   (let(
+;;        (kdr (gensym 'cdr))
+;;        (kar (gensym 'car)))
+;;     `(let ,kdr ()
+;;           (let(
+;;                (,kar (delay (read-char ,p))))
+;;             (cons
+;;              ,kar
+;;              (delay (begin (force ,kar) (,kdr))))))))
+
+;; (define-macro (stream-pos st)
+;;   `(error "position not supported by port parser"))
+
+;; (define-macro (stream-row st)
+;;   `(error "row not supported by port parser"))
+
+;; (define-macro (stream-col st)
+;;   `(error "column not supported by port parser"))
+
+(define-macro (stream p)
+  `(vector #f #f ,p))
 
 (define-macro (stream-car p)
-  `(force (car ,p)))
+  (let(
+       (ch (gensym 'ch)))
+    `(or (vector-ref ,p 0)
+	 (let(
+	      (,ch (read-char (vector-ref ,p 2))))
+	   (vector-set! ,p 0 ,ch)
+	   ,ch))))
 
 (define-macro (stream-cdr p)
-  `(force (cdr ,p)))
+  (let(
+       (kdr (gensym 'kdr)))
+    `(or (vector-ref ,p 1)
+	 (and (stream-car ,p)
+	      (let(
+		   (,kdr (vector #f #f (vector-ref ,p 2))))
+		(vector-set! ,p 1 ,kdr)
+		,kdr)))))
 
 (define-macro (stream-pos st)
   `(error "position not supported by port parser"))
@@ -22,16 +63,5 @@
 (define-macro (stream-col st)
   `(error "column not supported by port parser"))
 
-(define-macro (stream p)
-  (let(
-       (kdr (gensym 'cdr))
-       (kar (gensym 'car)))
-    `(let ,kdr ()
-          (let(
-               ;;(,kar (delay (let ((c (read-char ,p))) (display c (current-error-port)) c))))
-               (,kar (delay (read-char ,p))))
-            (cons
-             ,kar
-             (delay (begin (force ,kar) (,kdr))))))))
 
-  
+	       
--- src/ansuz/sources/port-with-position#.scm
+++ src/ansuz/sources/port-with-position#.scm
@@ -6,42 +6,79 @@
               stream-col
               stream))
 
+;; (define-macro (stream-car p)
+;;   `(vector-ref ,p 0))
+
+;; (define-macro (stream-cdr p)
+;;   (let(
+;;        (v (gensym 'v)))
+;;     `(or (vector-ref ,p 1)
+;;          (let(
+;;               (,v ((vector-ref ,p 2))))
+;;            (vector-set! ,p 1 ,v)
+;;            ,v))))
+
+;; (define-macro (stream-pos p)
+;;   (vector-ref ,p 3))
+
+;; (define-macro (stream-row p)
+;;   `(vector-ref ,p 4))
+
+;; (define-macro (stream-col p)
+;;   `(vector-ref ,p 5))
+  
+;; (define-macro (stream p)
+;;   (let(
+;;        (str (gensym 'str))
+;;        (pos (gensym 'pos))
+;;        (row (gensym 'row))
+;;        (col (gensym 'col))
+;;        (ch (gensym 'ch)))
+;;     `(let ,str ((,pos 0) (,row 1) (,col 0))
+;;           (let(
+;;                (,ch (read-char ,p)))
+;;             (vector
+;;              ,ch
+;;              #f
+;;              (lambda ()
+;;                (if (or (eof-object? ,ch) (eq? ,ch #\newline))
+;;                    (,str (+ ,pos 1) (+ ,row 1) 0)
+;;                    (,str (+ ,pos 1) ,row (+ ,col 1))))
+;;              ,pos
+;;              ,row
+;;              ,col)))))
+
+(define-macro (stream p)
+  `(vector #f #f 0 1 0 ,p))
 
 (define-macro (stream-car p)
-  `(force (vector-ref ,p 0)))
+  (let(
+       (ch (gensym 'ch)))
+    `(or (vector-ref ,p ,0)
+	 (let(
+	      (,ch (read-char (vector-ref ,p 5))))
+	   (vector-set! ,p 0 ,ch)
+	   ,ch))))
 
 (define-macro (stream-cdr p)
-  `(force (vector-ref ,p 1)))
-
-(define-macro (stream-pos p)
-  (vector-ref ,p 2))
-
-(define-macro (stream-row p)
-  `(vector-ref ,p 3))
-
-(define-macro (stream-col p)
-  `(vector-ref ,p 4))
-  
-(define-macro (stream p)
   (let(
-       (str (gensym 'str))
-       (pos (gensym 'pos))
-       (row (gensym 'row))
-       (col (gensym 'col))
        (ch (gensym 'ch))
-       (c (gensym 'ch)))
-    `(let ,str ((,pos 0) (,row 1) (,col 0))
-       (let(
-            (,ch (delay (read-char ,p))))
-       (vector
-        ,ch
-        (delay
-          (let(
-               (,c (force ,ch)))
-            ;;(display ,c (current-error-port))
-            (if (or (eof-object? ,c) (eq? ,c #\newline))
-                (,str (+ ,pos 1) (+ ,row 1) 0)
-                (,str (+ ,pos 1) ,row (+ ,col 1)))))
-        ,pos
-        ,row
-        ,col)))))
+       (kdr (gensym 'kdr)))
+    `(or (vector-ref ,p 1)
+	 (let*(
+	       (,ch (stream-car ,p))
+	       (,kdr (if (or (eof-object? ,ch) (char=? ,ch #\newline))
+			 (vector #f #f (+ (vector-ref ,p 2) 1) (+ (vector-ref ,p 3) 1) 0 (vector-ref ,p 5))
+			 (vector #f #f (+ (vector-ref ,p 2) 1) (vector-ref ,p 3) (+ 1 (vector-ref ,p 4)) (vector-ref ,p 5)))))
+	   (vector-set! ,p 1 ,kdr)
+	   ,kdr))))
+
+(define-macro (stream-pos st)
+  `(vector-ref ,st 2))
+
+(define-macro (stream-row st)
+  `(vector-ref ,st ,3))
+
+(define-macro (stream-col st)
+  `(vector-ref ,st 4))
+			 
